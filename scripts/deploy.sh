e40fb872bffccf4f17d9971fa31d461dabc3a102
ntno <ntno@users.noreply.github.com>
2022-11-23 16:59:20 -0500

    Refactor makefile /  deploy process (#54)
    * condense dockercompose files
    
    * add deploy scripts
    
    * updated scripts / makefile for project
    
    * robot update

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# example use: source deploy.sh dev us-east-1
+# $1 - environment name (ex: dev, prod)
+# $2 - datacenter/region name (ex: us-east-1, us-west-2)
+
+## build
+source ./scripts/build.sh $1 $2
+
+## deploy
+eval "$(buildenv -e $1 -d $2)" && \
+aws s3 sync --size-only --sse AES256 --acl public-read ./site/ "s3://$DOMAIN_NAME"
+
+make invalidate-distribution env=$1 region=$2
---
+++ scripts/serve.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# example use: source serve.sh dev us-east-1
+# $1 - environment name (ex: dev, prod)
+# $2 - datacenter/region name (ex: us-east-1, us-west-2)
+
+eval "$(buildenv -e $1 -d $2)" && \
+mkdocs serve -v --dev-addr=0.0.0.0:7000
